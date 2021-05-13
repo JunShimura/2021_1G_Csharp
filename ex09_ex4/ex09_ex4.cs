3ee46d91cb057a9846ee440016aed9bdc19ac562
Jun Shimura <junshimura@gmail.com>
2021-05-13 15:51:20 +0900

    LoopSample

---
+++ ex09_ex4/ex09_ex4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex09_ex4
+{
+    class ex09_ex4
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            var com = rand.Next(0, 3);
+            Console.WriteLine("★じゃんけんゲーム★\n手を入力してね\n(0=ぐう、1=ちょき、2=ぱあ)");
+            var player = int.Parse(Console.ReadLine());
+            Console.WriteLine($"私は{com}、あなたは{player}");
+            //ここで勝ち負けの判定を行いメッセージを出力
+            var judge = (com - player + 3) % 3;
+            string[] message = { "あいこです", "あなたの勝ちです", "あなたの負けです" };
+            Console.WriteLine(message[judge]);
+
+        }
+    }
+}
+
---
+++ ex09_ex5/ex09_ex5.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ex09_ex5
+{
+    class ex09_ex5
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            var cpu = rand.Next(0, 3);
+            Console.WriteLine("★じゃんけんゲーム★\n手を入力してね\n(0=グー、1=チョキ、2=パー)");
+            var player = int.Parse(Console.ReadLine());
+            Console.WriteLine($"私は{cpu}、あなたは{player}");
+            //ここで勝ち負けの判定を行いメッセージを出力
+            switch (cpu)
+            {
+                case 0: //　cpuがぐう
+                    switch (player)
+                    {
+                        case 0://プレイヤーがぐう
+                            Console.WriteLine("あいこです");
+                            break;
+                        case 1:
+                            //プレイヤーがちょき
+                            Console.WriteLine("あなたの負け");
+                            break;
+                        case 2:
+                            //プレイヤーがぱあ
+                            Console.WriteLine("あなたの勝ち");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 1:    //　cpuがちょき
+                    switch (player)
+                    {
+                        case 0://プレイヤーがぐう
+                            Console.WriteLine("あなたの勝ち");
+                            break;
+                        case 1: //プレイヤーがちょき
+                            Console.WriteLine("あいこです");
+                            break;
+                        case 2: //プレイヤーがぱあ
+                            Console.WriteLine("あなたの負け");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 2://　cpuがぱあ
+                    switch (player)
+                    {
+                        case 0://プレイヤーがぐう
+                            Console.WriteLine("あなたの負け");
+                            break;
+                        case 1: //プレイヤーがちょき
+                            Console.WriteLine("あなたの勝ち");
+                            break;
+                        case 2: //プレイヤーがぱあ
+                            Console.WriteLine("あいこです");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
