dbd15d28b127bc48513dee682b1977a7547efa86
Jun Shimura <junshimura@gmail.com>
2021-05-20 13:14:46 +0900

    ex11

---
+++ ex11/ex11.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex11
+{
+    class ex11
+    {
+        static void Main(string[] args)
+        {
+            var x = int.Parse(Console.ReadLine());
+            if (x >= 1 && x <= 20)
+            {
+                for (var n = 1; n <= x; n++) //高さ
+                {
+                    for (var i = 1; i <= n; i++) //行ごとの幅が何行目かと一致する
+                    {
+                        Console.Write("＊");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("入力エラー");
+            }
+        }
+    }
+}
---
+++ ex11_hint/ex11_hint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex11_hint
+{
+    class ex11_hint
+    {
+        static void Main(string[] args)
+        {
+            var x = int.Parse(Console.ReadLine());
+            for (var n = 1; n <= x; n++) //高さ
+            {
+                for (var i = 1; i <= x; i++) //行ごとの幅
+                {
+                    Console.Write("＊");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
