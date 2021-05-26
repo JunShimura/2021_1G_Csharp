2368dee6e46fa2653cff90a3fee0257176983418
Jun Shimura <junshimura@gmail.com>
2021-05-26 12:11:17 +0900

    ex12_ex2

--- ex12_ex2/ex12_ex2.cs
+++ ex12_ex2/ex12_ex2.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-namespace ex12
+namespace ex12_ex2
 {
-    class ex12
+    class ex12_ex2
     {
         static void Main(string[] args)
         {
@@ -19,49 +19,55 @@ namespace ex12
             if (width >= min && width <= max && height >= min && height <= max)
             {
                 Console.WriteLine("1.長方形");
+                var s = "";
+                for (var j = 1; j <= width; j++) //行ごとの幅
+                {
+                    s += char1;
+                }
                 for (var i = 1; i <= height; i++) //高さ
                 {
-                    var s = "";
-                    for (var j = 1; j <= width; j++) //行ごとの幅
-                    {
-                        s += char1;
-                    }
                     Console.WriteLine(s);
                 }
                 Console.WriteLine("2.よこ縞");
+                var s0 = "";
+                for (var j = 1; j <= width; j++) //行ごとの幅
+                {
+                    s0 += char1;
+                }
+                var s1 = "";
+                for (var j = 1; j <= width; j++) //行ごとの幅
+                {
+                    s1 += char2;
+                }
                 for (var i = 1; i <= height; i++) //高さ
                 {
-                    var s = "";
-                    for (var j = 1; j <= width; j++) //行ごとの幅
+                    if (i % 2 == 0)
                     {
-                        if (i % 2 == 0)
-                        {
-                            s += char1;
-                        }
-                        else
-                        {
-                            s += char2;
-                        }
+                        Console.WriteLine(s0);
                     }
-                    Console.WriteLine(s);
+                    else
+                    {
+                        Console.WriteLine(s1);
+                    }
                 }
                 Console.WriteLine("3.縦縞");
+                s = "";
+                for (var j = 1; j <= width; j++) //行ごとの幅
+                {
+                    if (j % 2 == 0)
+                    {
+                        s += char1;
+                    }
+                    else
+                    {
+                        s += char2;
+                    }
+                }
                 for (var i = 1; i <= height; i++) //高さ
                 {
-                    var s = "";
-                    for (var j = 1; j <= width; j++) //行ごとの幅
-                    {
-                        if (j % 2 == 0)
-                        {
-                            s += char1;
-                        }
-                        else
-                        {
-                            s += char2;
-                        }
-                    }
-                    Console.WriteLine(s);
+                        Console.WriteLine(s);
                 }
+                /*
                 Console.WriteLine("4.中抜け");
                 for (var i = 1; i <= height; i++) //高さ
                 {
@@ -83,7 +89,7 @@ namespace ex12
                 for (var i = 1; i <= height; i++) //高さ
                 {
                     var s = "";
-                    var length = (float)i / height * width;
+                    float length = (float)i / height * width;
                     for (var j = 1; j <= length; j++) //行ごとの幅
                     {
                         s += char1;
@@ -107,6 +113,7 @@ namespace ex12
                     }
                     Console.WriteLine(s);
                 }
+                */
             }
             else
             {
@@ -115,3 +122,4 @@ namespace ex12
         }
     }
 }
+
