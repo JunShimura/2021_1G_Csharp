1bdc4641efe5c4dbf47c2893c7adee1267c46abb
Jun Shimura <junshimura@gmail.com>
2021-06-09 11:25:04 +0900

    diemntionSample

---
+++ DimentionSample/DimentionSample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DimentionSample
+{
+    class DimentionSample
+    {
+        static void Main(string[] args)
+        {
+            //　配列でとる
+            const int numMember = 5;
+            double[] tall= new double[numMember];
+            //身長5人分を入力
+            /*
+            Console.Write("0番目の身長:");
+            tall[0] = int.Parse(Console.ReadLine());
+            Console.Write("1番目の身長:");
+            tall[1] = int.Parse(Console.ReadLine());
+            */
+            for (var i = 0; i < numMember; i++)
+            {
+                Console.Write($"{i}番目の身長:");
+                tall[i] = double.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ DimentionSampleNew/DimentionSampleNew.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DimentionSampleNew
+{
+    class DimentionSampleNew
+    {
+        static void Main(string[] args)
+        {
+            int[] coin = { 10000, 5000, 1000, 500, 100, 50, 10, 5, 3 };
+            //金種をプログラムで追加したい場合
+            //coin =  { 10000, 5000, 2000, 1000, 500, 100, 50, 10, 5, 1 };//エラー
+            //new で新たに全部を作り直すとエラーにならない
+            //coin =  new int[]{ 10000, 5000, 2000, 1000, 500, 100, 50, 10, 5, 1 };//正常
+
+            Console.Write("何円にしますか：");
+            var changeCoin = int.Parse(Console.ReadLine());
+            int[] coin2 = new int[coin.Length + 1];
+            int i = 0, j = 0;
+            var copied = false;
+            for (; i < coin2.Length; i++)
+            {
+                if (!copied && (i == coin.Length || coin[i] < changeCoin))
+                {
+                    coin2[i] = changeCoin;
+                    copied = true;
+                }
+                else
+                {
+                    coin2[i] = coin[j];
+                    j++;
+                }
+            }
+            /*            int i;
+                        for (i = 0; i < coin.Length; i++)
+                        {
+                            if (coin[i] > changeCoin)
+                            {
+                                coin2[i] = coin[i];
+                            }
+                            else
+                            {
+                                coin2[i] = changeCoin;
+                                break;
+                            }
+                        }
+                        if (i == coin.Length)
+                        {   //最後まで見つからなかった
+                            coin2[i] = changeCoin;
+                        }
+                        else
+                        {
+                            for (int k = i + 1; k < coin2.Length; k++)
+                            {
+                                coin2[k] = coin[k - 1];
+                            }
+                        }*/
+        }
+    }
+}
