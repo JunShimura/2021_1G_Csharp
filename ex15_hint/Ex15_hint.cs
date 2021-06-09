3be8dd93eee763e6d333e39abaa9ee230a88f346
Jun Shimura <junshimura@gmail.com>
2021-06-09 12:13:43 +0900

    ex15_hint

---
+++ ex15_hint/Ex15_hint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex15_hint
+{
+    class Ex15_hint
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("数を入力：");
+            var inputNumber = int.Parse(Console.ReadLine());
+            string[] words0to19 =
+                {
+                    "zero", "one",  "two",  "three",    "four", "five", "six",  "seven",    "eight",    "nine",
+                    "ten",  "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"
+                };
+            if(inputNumber>=0 && inputNumber <= 99)
+            {
+                Console.WriteLine(words0to19[inputNumber]);
+            }
+            else
+            {
+                Console.WriteLine("入力エラー");
+            }
+        }
+    }
+}
