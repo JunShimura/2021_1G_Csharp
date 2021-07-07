2c6c4c69acdfdd93d7eff65eba95327b8dfc1831
Jun Shimura <junshimura@gmail.com>
2021-07-07 11:44:18 +0900

    20waves hint

---
+++ Challenge02_hint_FYevolve/Challenge02_hint_FYevolve.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Challenge02_hint_FYevolve
+{
+    class Challenge02_hint_FYevolve
+    {
+        static void Main(string[] args)
+        {
+            char[] tetrimino = { 'I', 'O', 'S', 'Z', 'J', 'L', 'T' };      //元
+            Random random = new Random();
+
+            for (var i = 0; i < tetrimino.Length-1; i++)
+            {
+                var source = random.Next(i+1, tetrimino.Length);    //入れ替える相手の指標
+                //　交換
+                var temp = tetrimino[i];    
+                tetrimino[i] = tetrimino[source];
+                tetrimino[source] = temp;
+            }
+            Console.WriteLine(tetrimino);
+        }
+    }
+}
---
+++ Challenge02_hint_FYevolve_20wave_hint/Challenge02_hint_FYevolve_20wave_hint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Challenge02_hint_FYevolve_20wave_hint
+{
+    class Challenge02_hint_FYevolve_20wave_hint
+    {
+        const int waveCount = 20;
+        static void Main(string[] args)
+        {
+            char[] tetrimino = { 'I', 'O', 'S', 'Z', 'J', 'L', 'T' };      //元
+            char[,] wave = new char[waveCount, tetrimino.Length];
+
+            Random random = new Random();
+
+            for (var i = 0; i < waveCount; i++)
+            {
+                //　元からコピー
+                for (var j = 0; j < tetrimino.Length; j++)
+                {
+                    wave[i, j] = tetrimino[j];
+                }
+                //　1パターンを作る
+                for (var j = 0; j < tetrimino.Length - 1; j++)
+                {
+                    var source = random.Next(j + 1, tetrimino.Length);    //入れ替える相手の指標
+                                                                          //　交換
+                    var temp = wave[i, j];
+                    wave[i, j] = wave[i, source];
+                    wave[i, source] = temp;
+                }
+                // wave[0,*]～wave[i-1,*]で重複が在るか確認、重複があった場合はランダムのやり直し
+
+                Console.Write($"\n{(i + 1).ToString("00")}番目");
+                for (var j = 0; j < tetrimino.Length; j++)
+                {
+                    Console.Write(wave[i, j].ToString());
+                }
+            }
+        }
+    }
+}
+
