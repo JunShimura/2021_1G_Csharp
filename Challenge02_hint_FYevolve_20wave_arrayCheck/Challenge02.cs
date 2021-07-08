1210ff786e698d9559b1e93765395f83dd832b1f
Jun Shimura <junshimura@gmail.com>
2021-07-08 15:53:00 +0900

    Answered with a raw dimention

--- Challenge02_hint_FYevolve_20wave_arrayCheck/Challenge02.cs
+++ Challenge02_hint_FYevolve_20wave_arrayCheck/Challenge02.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 
-namespace Challenge02_hint_FYevolve_20wave_arrayCheck
-{
-    class Challenge02_hint_FYevolve_20wave_arrayCheck
+namespace Challenge02 {
+    class Challenge02
     {
-        const int waveCount = 20;
+        const int waveCount = 1000;
         static void Main(string[] args)
         {
             char[] tetrimino = { 'I', 'O', 'S', 'Z', 'J', 'L', 'T' };      //元
@@ -25,7 +24,7 @@ namespace Challenge02_hint_FYevolve_20wave_arrayCheck
                     //　1パターンを作る
                     for (var j = 0; j < tetrimino.Length - 1; j++)
                     {
-                        var source = random.Next(j + 1, tetrimino.Length);    //入れ替える相手の指標
+                        var source = random.Next(j , tetrimino.Length);    //入れ替える相手の指標
                                                                               //　交換
                         var temp = wave[i, j];
                         wave[i, j] = wave[i, source];
@@ -33,18 +32,23 @@ namespace Challenge02_hint_FYevolve_20wave_arrayCheck
                     }
                     // wave[0,*]～wave[i-1,*]で重複が在るか確認、重複があった場合はランダムのやり直し
                     var k = 0;
-                    for(; k < i; k++)
+                    for (; k < i; k++)
                     {
-                        for(var j = 0; j < tetrimino.Length; j++)
+                        var j = 0;
+                        for (; j < tetrimino.Length; j++)
                         {
                             if (wave[i, j] != wave[k, j])
                             {
                                 break;
                             }
                         }
+                        if (j == tetrimino.Length)
+                        {   // 同じ要素を発見
+                            break;
+                        }
                     }
                     if (k == i)
-                    {
+                    {   //　同じ要素がない
                         sucess = true;
                     }
                 }
