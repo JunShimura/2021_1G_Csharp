c47aa99bd3df2532c8b9029fcbd3ff28a86460bb
Jun Shimura <junshimura@gmail.com>
2021-07-21 09:39:29 +0900

    Challenge01

---
+++ Challenge01_hint/Challenge01_hint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Challenge01_hint
+{
+    class Challenge01_hint
+    {
+        //　進化に必要な飴の数
+        const int evolvable = 12;
+
+        static void Main(string[] args)
+        {
+            Console.Write("ポッポは何匹？");
+            var poppo = int.Parse(Console.ReadLine());
+            Console.Write("飴は何個？");
+            var candy = int.Parse(Console.ReadLine());
+            var evolve = 0; // 進化した数
+
+        }
+    }
+}
