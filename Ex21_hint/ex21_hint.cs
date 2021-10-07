43c065a43abbbb272b28fa8df09b0a40c4519a94
Jun Shimura <junshimura@gmail.com>
2021-10-07 16:32:03 +0900

    ex21

---
+++ Ex21_hint/ex21_hint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex21_hint
+{
+    class ex21_hint
+    {
+
+        static void Main(string[] args)
+        {
+            string s1 = InputString("名前を入力してください");
+            int no = InputNumber("ほめ指数を入力してください");
+            string s2 = Praise(s1, no);
+            Console.WriteLine(s2);
+        }
+        static string Praise(string s, int praiseIndex)
+        {
+            //ここに処理を追加
+            string answer = null;
+            return answer;
+        }
+        static int InputNumber(string message)
+        {
+            Console.WriteLine(message);
+            int i = int.Parse(Console.ReadLine());
+            return i;
+        }
+        static string InputString(string message)
+        {
+            Console.WriteLine(message);
+            var i = Console.ReadLine();
+            return i;
+        }
+
+    }
+}
