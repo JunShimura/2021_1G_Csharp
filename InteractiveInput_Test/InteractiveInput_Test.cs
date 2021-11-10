dd73cd5352c2a787f9cf8ef5d424a3dc5d834867
Jun Shimura <junshimura@gmail.com>
2021-11-10 10:53:03 +0900

    InteractiveInput

---
+++ InteractiveInput_Test/InteractiveInput_Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using InteractiveInput;
+
+namespace InteractiveInput_Test
+{
+    class InteractiveInput_Test
+    {
+        static void Main(string[] args)
+        {
+            float f = InteractiveInput. InputFloat();
+            Console.WriteLine($"入力値は{f}");
+            f = InteractiveInput.InputFloat("文字列指定：値を入れてください");
+            Console.WriteLine($"入力値は{f}");
+            f = InteractiveInput.InputFloat(1.5f, "指定文字列：値を入力");
+            Console.WriteLine($"入力値は{f}");
+            f = InteractiveInput.InputFloat(999.9f, -20);
+            Console.WriteLine($"入力値は{f}");
+        }
+    }
+}
