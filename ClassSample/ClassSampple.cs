aef6182936e56e3d87bf1dbaa2fe4326156e48b2
Jun Shimura <junshimura@gmail.com>
2021-11-11 17:26:10 +0900

    Box Class

---
+++ ClassSample/ClassSampple.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassSample
+{
+    class ClassSampleMain
+    {
+        static void Main(string[] args)
+        {
+            ClassSampleA classSampeA = new ClassSampleA(3,8);    // インスタンス
+            var a = classSampeA.Func();
+            //classSampeA.a = 5; //エラーになる
+            Console.WriteLine(a);
+        }
+    }
+    class ClassSampleA   //　Classの宣言
+    {
+        int a;
+        int b;
+        public ClassSampleA(int a,int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int Func()
+        {
+            return a + b;
+        }
+    }
+}
