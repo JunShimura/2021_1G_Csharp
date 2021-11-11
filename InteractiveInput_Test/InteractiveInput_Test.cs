c52f5e837b47f20cf8feba8a5738330a2a9f3b69
Jun Shimura <junshimura@gmail.com>
2021-11-11 14:28:43 +0900

    Interactive

--- InteractiveInput_Test/InteractiveInput_Test.cs
+++ InteractiveInput_Test/InteractiveInput_Test.cs
@@ -1,32 +1,25 @@
 ﻿using System;
-<<<<<<< HEAD
 using ConsoleUtility;
 using NumberUtility;
 
-namespace InteractiveInputNameSpace
-=======
-using InteractiveInput;
-
-namespace InteractiveInput_Test
->>>>>>> d9d847198202e3e4940240df6d9a3b2988f3e090
-{
+namespace interactiveInput_test{
     class InteractiveInput_Test
     {
         static void Main(string[] args)
         {
             float f = InteractiveInput. InputFloat();
-<<<<<<< HEAD
+
             ConsoleOut.OutputColoredText($"入力値は{f}",ConsoleColor.Yellow);
             f = InteractiveInput.InputFloat("文字列指定：値を入れてください");
             Console.WriteLine($"入力値は{f}");
             ConsoleOut.OutputColoredText(
                 $"入力値が自然数かは{NumberCheck.IsNaturalNumber(f)}",
                 ConsoleColor.DarkYellow);
-=======
+
             Console.WriteLine($"入力値は{f}");
             f = InteractiveInput.InputFloat("文字列指定：値を入れてください");
             Console.WriteLine($"入力値は{f}");
->>>>>>> d9d847198202e3e4940240df6d9a3b2988f3e090
+
             f = InteractiveInput.InputFloat(1.5f, "指定文字列：値を入力");
             Console.WriteLine($"入力値は{f}");
             f = InteractiveInput.InputFloat(999.9f, -20);
