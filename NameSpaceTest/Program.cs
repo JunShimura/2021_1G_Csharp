09dae51b86f65a2e23997082fb3ee358517f1069
Jun Shimura <junshimura@gmail.com>
2021-11-29 16:10:08 +0900

    NameSpaceTest

---
+++ NameSpaceTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Space1
+{
+    class Class1
+    {
+        public static void test()
+        {
+            Console.WriteLine("Space1.Class1.test() called");
+        }
+    }
+}
+
+namespace Space1
+{
+    class Class2
+    {
+        public static void test()
+        {
+            Console.WriteLine("Space1.Class2.test() called");
+        }
+    }
+}
+
+namespace ConsoleApplication21
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Space1.Class1.test();
+            Space1.Class2.test();
+        }
+    }
+}
