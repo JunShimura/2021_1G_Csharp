899bc10704990fa04cd24af149b51a1c45060535
Jun Shimura <junshimura@gmail.com>
2021-12-15 09:41:40 +0900

    StaticFunction

--- StaticSample/StaticSampleMain.cs
+++ StaticSample/StaticSampleMain.cs
@@ -7,11 +7,12 @@ namespace StaticSample
         static int sA = 5;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             int a = 5;  // int型のインスタンス、a
             // TestClass1型のインスタンス、testClass1
-            TestClass1 testClass1A = new TestClass1(a);
-            TestClass1 testClass1B = new TestClass1(a);
+            //TestClass1 testClass1A = new TestClass1(a);
+            //TestClass1 testClass1B = new TestClass1(a);
+            TestClass2.a = 3;
+            int ans = TestClass2.GetAAA();
         }
     }
 }
