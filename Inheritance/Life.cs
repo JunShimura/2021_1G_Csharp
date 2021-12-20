dd0f6a0f3ee6ff434a5ef7c8e78f35ad17658b4b
Jun Shimura <junshimura@gmail.com>
2021-12-20 17:28:09 +0900

    Inheritance

---
+++ Inheritance/Life.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Life:Thing
+    {
+        readonly private string _name;
+        readonly private DateTime _birthDay = new DateTime();
+        public DateTime birthday
+        {
+            get { return _birthDay; }
+        }
+        public string name
+        {
+            get { return _name; }
+        }
+        public Life(DateTime bd ,string n, float  w = 0, float h = 0, float d = 0 ):base(n,w,h,d)
+        {
+            this._birthDay = bd;
+        }
+    }
+}
