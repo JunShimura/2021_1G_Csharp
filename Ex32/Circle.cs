2e82c24e32b499cd0a0e192d10b18d41835f38c9
Jun Shimura <junshimura@gmail.com>
2022-02-17 12:06:20 +0900

    Abstract

---
+++ Ex32/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex32
+{
+    class Circle : Surface
+    {
+        readonly public float radius;
+        public Circle(float radius = 0)
+        {
+            this.radius = radius;
+        }
+        public override float GetSurface()
+        {
+            return radius * radius * MathF.PI;
+        }
+        public override float GetCircumference()
+        {
+            return radius * 2 * MathF.PI;
+        }
+        public override void GetBounds(out float width, out float height)
+        {
+            width = this.radius;
+            height = this.radius;
+        }
+    }
+}
